540d4e2d0481af19635579bff1a52d6140ca999a
Younghwan90s <younghwan1608@gmail.com>
2024-01-21 18:02:51 +0900

    add gitignore

--- obj/Debug/net6.0/RunNetCoreWeb.AssemblyInfo.cs
+++ obj/Debug/net6.0/RunNetCoreWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunNetCoreWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449faffac8d53b5b99109929bbc611f1fdb1ba7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc98d4d008d121fddfc528c9ac7dd4b14723632d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunNetCoreWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunNetCoreWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
